fbd392d3e338fb3d4a88637708e2073474b14f4d
orange1438 <orange1438@qq.com>
2017-02-19 00:08:55 +0800

    添加常量类
    Signed-off-by: orange1438 <orange1438@qq.com>

---
+++ src/main/java/indi/orange1438/managementsystem/util/Const.java
@@ -0,0 +1,13 @@
+package indi.orange1438.managementsystem.util;
+
+/**
+ * 常量类
+ *
+ * @author orange1438
+ *         github: github.com/orange1438
+ *         data: 2017/02/18 19:47
+ */
+public class Const {
+    // 验证码
+    public static final String SESSION_SECURITY_CODE = "sessionSecCode";
+}
