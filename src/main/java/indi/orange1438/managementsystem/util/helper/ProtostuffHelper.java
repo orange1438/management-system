3f8f5458e563107c45dd9ad9f9e68debf0b969f8
orange1438 <orange1438@qq.com>
2017-02-19 12:29:32 +0800

    增加序列化工具
    Signed-off-by: orange1438 <orange1438@qq.com>

---
+++ src/main/java/indi/orange1438/managementsystem/util/helper/ProtostuffHelper.java
@@ -0,0 +1,80 @@
+package indi.orange1438.managementsystem.util.helper;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+
+/**
+ * Protostuff序列化工具
+ *
+ * @author orange1438
+ *         github: github.com/orange1438
+ *         data: 2017/02/19 12:24
+ */
+public class ProtostuffHelper {
+    private static Map<Class<?>, Schema<?>> cachedSchema = new ConcurrentHashMap<Class<?>, Schema<?>>();
+
+    private static <T> Schema<T> getSchema(Class<T> clazz) {
+        @SuppressWarnings("unchecked")
+        Schema<T> schema = (Schema<T>) cachedSchema.get(clazz);
+        if (schema == null) {
+            schema = RuntimeSchema.getSchema(clazz);
+            if (schema != null) {
+                cachedSchema.put(clazz, schema);
+            }
+        }
+        return schema;
+    }
+
+    /**
+     * 序列化
+     *
+     * @param obj
+     * @return
+     */
+    public static <T> byte[] serialize(T obj) {
+        @SuppressWarnings("unchecked")
+        Class<T> clazz = (Class<T>) obj.getClass();
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        try {
+            Schema<T> schema = getSchema(clazz);
+            return ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    /**
+     * 反序列化
+     *
+     * @param data
+     * @param clazz
+     * @return
+     */
+    public static <T> T deserialize(byte[] data, Class<T> clazz) {
+        try {
+            T obj = clazz.newInstance();
+            Schema<T> schema = getSchema(clazz);
+            ProtostuffIOUtil.mergeFrom(data, obj, schema);
+            return obj;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String obj = "测试数据";
+
+        byte[] bytes = ProtostuffHelper.serialize(obj);
+        System.out.println("序列化后：" + bytes.toString());
+
+        String xxx = ProtostuffHelper.deserialize(bytes, String.class);
+        System.out.println("反序列化后：" + xxx);
+    }
+}
