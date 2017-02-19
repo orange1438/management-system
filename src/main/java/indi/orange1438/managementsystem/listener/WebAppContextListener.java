c3226e5f5bda2cfd5f9a8602634c11857f9892c8
orange1438 <orange1438@qq.com>
2017-02-19 18:30:49 +0800

    Listener例子
    Signed-off-by: orange1438 <orange1438@qq.com>

---
+++ src/main/java/indi/orange1438/managementsystem/listener/WebAppContextListener.java
@@ -0,0 +1,21 @@
+package indi.orange1438.managementsystem.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Listener例子
+ *
+ * @author orange1438
+ *         github: github.com/orange1438
+ *         data: 2017/02/19 18:22
+ */
+public class WebAppContextListener implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("listener initialized...");
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("listener destroyed....");
+    }
+}
