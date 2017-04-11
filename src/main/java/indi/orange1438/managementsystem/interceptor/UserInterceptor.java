ce22f177f9a01657e48ac102138fcf4ab88d1e2f
orange1438 <orange1438@qq.com>
2017-04-11 16:35:23 +0800

    拦截器增加细节
    Signed-off-by: orange1438 <orange1438@qq.com>

--- src/main/java/indi/orange1438/managementsystem/interceptor/UserInterceptor.java
+++ src/main/java/indi/orange1438/managementsystem/interceptor/UserInterceptor.java
@@ -1,5 +1,7 @@
 package indi.orange1438.managementsystem.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import indi.orange1438.managementsystem.dto.BaseResult;
 import indi.orange1438.managementsystem.util.Const;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -7,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 
 /**
  * 用户登录拦截器
@@ -27,7 +30,16 @@ public class UserInterceptor implements HandlerInterceptor {
         if (sessionObj != null) {
             return true;
         }
-        httpServletResponse.sendRedirect("toLogin");
+        System.out.println("no login:" + httpServletRequest.getMethod());
+        if ("POST".equalsIgnoreCase(httpServletRequest.getMethod())) {
+            httpServletResponse.setContentType("application/json;charset=utf-8");
+            PrintWriter out = httpServletResponse.getWriter();
+            out.write(JSON.toJSONString(new BaseResult(false, "未登录！")));
+            out.flush();
+            out.close();
+        } else {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "toLogin");
+        }
         return false;
     }
 
